92cd67f42d78ff240992f2186e52cabf2e07ed94
Bryan Choe <bryanchoe@Bryans-MacBook-Air.local>
2016-09-07 09:48:58 -1000

    challenge complete

--- multiples.js
+++ multiples.js
@@ -1,12 +1,22 @@
 /**
- * sumOfAMultiple() sums of the multiples of 3 and 5 for any number passed in
+ * sums of the multiples of 3 and 5 for any number passed in
  * @param  {Number} n Number that is to be used for calculating multiples up to that Number
  * @return {Number}
  */
 exports.sumOfAMultiple = function( n ) {
   var sum = 0;
+  for (var x=0; x<n; x++){
+  	if(x%3===0 || x%5===0){
+  		sum +=x;
+  	}
+  	
+  }
+
 
   // do your work here
 
   return sum;
+  	
 };
+console.log(exports.sumOfAMultiple(1000));
+console.log(exports.sumOfAMultiple(10000));
