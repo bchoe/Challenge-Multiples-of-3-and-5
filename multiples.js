94eacca821e3bc3593cc04738ab3b3a75c760751
Bryan Choe <bryanchoe@Bryans-MacBook-Air.local>
2016-09-23 19:06:56 -1000

    commit

--- multiples.js
+++ multiples.js
@@ -4,13 +4,13 @@
  * @return {Number}
  */
 exports.sumOfAMultiple = function( n ) {
-  var sum = 0;
-  for (var x=0; x<n; x++){
-  	if(x%3===0 || x%5===0){
-  		sum +=x;
-  	}
-  	
-  }
+var sum = 0;
+for (var x=0; x<n; x++){
+	if(x%3===0 || x%5===0){
+		   sum +=x;
+	}
+	
+}
 
 
   // do your work here
